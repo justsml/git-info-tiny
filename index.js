02e8c82c0b8e37cbabbcc65bf2f6cd24271d2836
Dan Levy <justsml@gmail.com>
2014-12-19 13:58:01 -0700

    Updated response fields & tests updated, run make test

--- index.js
+++ index.js
@@ -6,22 +6,21 @@
  * Dan Levy, Damian Krawczyk, Corey Jewett, Maciej Lisiewski, Tom Gault, kolya-ay, nerdgore, madarche, drasive
  * Licensed under the MIT license.
  */
-
-// JSLint
 /*jslint node: true */
 
 
 module.exports = function (options, callback) {
 	'use strict'
 	var	path = require('path'),
-			workQueue, task, done, commands;
+			summary = {},
+			hasFailed = false, workQueue, task, done, commands;
 
 	options = options || {}
 	// default to THIS file's *parent*'s path
 	options.cwd = options.path || path.resolve(__dirname, '..')
 
 	commands = {
-		'name'              : ['rev-parse', '--abbrev-ref', 'HEAD'],
+		'currentBranch'     : ['rev-parse', '--abbrev-ref', 'HEAD'],
 		'SHA'               : ['rev-parse', 'HEAD'],
 		'tinySHA'           : ['rev-parse', '--short', 'HEAD'],
 		'smallSHA'          : ['rev-parse', '--short=16', 'HEAD'],
@@ -33,15 +32,22 @@ module.exports = function (options, callback) {
 		'remoteUrl'					: ['config', '--get-all', 'remote.origin.url']
 	}
 	task = function(key, cb) {
+		// short cut out if we already have error
+		if ( hasFailed ) { return cb(null, null); }
 		var results = [],
 			args = commands[key],
 			proc = require('child_process').spawn('git', args, {cwd: options.cwd })
 		proc.stdout.on('data', function(data) {
 			results.push(data)
 		})
-		proc.on('error', cb) // pass the err direct to the async callback
-		proc.stdout.on('close', function(data) {
-			cb(null, {key: key, results: results.join('').replace(/\n|\r$/, '') })
+		proc.on('close', function(exitCode, signal) {
+			if ( exitCode > 0 ) { 
+				hasFailed = true;
+				return cb(new Error('Path not a valid git project. Exit code: ' + exitCode + ' ' + signal)) 
+			}
+			var cmdResp = results.join('').replace(/\n|\r$/, '');
+			summary[key] = cmdResp
+			cb(null, cmdResp)
 		})
 	}
 	workQueue = Array.prototype.map.call(Object.keys(commands), function(key) { 
@@ -51,8 +57,8 @@ module.exports = function (options, callback) {
 	})
 	// Just hoping this speeds up processing in build & test scripts
 	// ... I might get rid of the async dependancy
-	require('async').parallelLimit(workQueue, 4, function(err, results) {
-		callback(err, results)
+	require('async').parallelLimit(workQueue, 4, function(err, resultsList) {
+		callback(err, summary)
 	})
 		
 }
--- test/test.js
+++ test/test.js
@@ -1,16 +1,25 @@
 var assert = require('better-assert'),
-		gitInfo = require('..')
+		gitInfo = require('..'),
+		path = require('path')
 
 
 
 describe('git-info-tiny', function() {
-	
+	it('fails gracefully', function(done) {
+		gitInfo({path: '/'}, function(err, summary) {
+			console.log(err, summary)
+			assert(err)
+			assert(err.message.indexOf('Path') > -1)
+			assert(!summary || !summary.SHA)
+			done()
+		});
+	})
 	it('has all data', function(done) {
-		gitInfo({}, function(err, summary) {
-			console.log(summary)
-			assert(summary.shortSHA.length >= 16)
+		gitInfo({path: path.resolve(__dirname, '..')}, function(err, summary) {
+			console.log(arguments)
+			assert(summary.smallSHA.length >= 16)
 			assert(summary.tinySHA.length >= 5)
-			assert(summary.name.length > 1)
+			assert(summary.currentBranch.length > 1)
 			assert(summary.SHA.length >= 40)
 			assert(summary.currentUser)
 			assert(summary.lastCommitTime)
